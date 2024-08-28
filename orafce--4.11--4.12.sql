f5cf947f1817ce138056ce146ca3663b07be5d87
Gilles Darold <gilles@darold.net>
2024-08-28 21:29:10 +0200

    Fix wrong number of days add to julian day in to_char(text, text) function and add pg_catalog FQDN to the to_date() builtin function called. Add regression test.

--- orafce--4.11--4.12.sql
+++ orafce--4.11--4.12.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION oracle.to_date (TEXT, TEXT) RETURNS oracle.date AS $$
 SELECT CASE WHEN upper($2) = 'J' THEN
-    to_date($1, $2) + '430 days'::interval
+    pg_catalog.to_date($1, $2) + '400 days'::interval
   ELSE
     TO_TIMESTAMP($1,$2)::oracle.date
   END;
