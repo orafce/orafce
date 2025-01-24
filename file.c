orafce/orafce

commit ae9d0302be607f86bdfecbf24e962678e6282e75

Allow to set umask for utl_file.fopen by setting orafce.umask. This setting is allowed only for user with rights of role orafce_set_umask.

file.c
@@ -27,6 +27,7 @@
 #include "miscadmin.h"
 #include "port.h"
 #include "storage/fd.h"
+#include "utils/acl.h"
 #include "utils/builtins.h"
 #include "utils/memutils.h"
 
@@ -124,6 +125,79 @@ static void check_secure_locality(const char *path);
 static char *get_safe_path(text *location, text *filename);
 static int copy_text_file(FILE *srcfile, FILE *dstfile,
 						  int start_line, int end_line);
+
+static int orafce_umask = 077;
+char *orafce_umask_str = NULL;
+
+static Oid orafce_set_umask_roleid = InvalidOid;
+
+void
+orafce_umask_assign_hook(const char *newvalue, void *extra)
+{
+	orafce_umask = *((int *) extra);
+}
+
+bool
+orafce_umask_check_hook(char **newval, void **extra, GucSource source)
+{
+	int			digits = 0;
+	char	   *ptr = *newval;
+	int		   *myextra;
+
+	if (IsNormalProcessingMode())
+	{
+		if (!superuser())
+		{
+			if (!OidIsValid(orafce_set_umask_roleid))
+				orafce_set_umask_roleid = get_role_oid("orafce_set_umask", false);
+
+			if (!has_privs_of_role(GetUserId(), orafce_set_umask_roleid))
+			{
+				GUC_check_errcode(ERRCODE_INSUFFICIENT_PRIVILEGE);
+				GUC_check_errmsg("permission denied to set \"orafce.umask\"");
+				GUC_check_errdetail("Only roles with privileges of the \"orafce_set_umask\" can set \"orafce.umask\".");
+
+				return false;
+			}
+		}
+	}
+
+	while (*ptr)
+	{
+		if (*ptr < '0' || *ptr > '7')
+		{
+			GUC_check_errdetail("invalid octal digit");
+			return false;
+		}
+
+		if (digits > 3)
+		{
+			GUC_check_errdetail("number is too big (only four digits are allowed");
+			return false;
+		}
+
+		ptr++;
+	}
+
+#if PG_VERSION_NUM >=  160000
+
+	myextra = (int *) guc_malloc(LOG, sizeof(int));
+
+#else
+
+	myextra = (int *) malloc(sizeof(int));
+
+#endif
+
+	if (!myextra)
+		return false;
+
+	*myextra = (int) strtol(*newval, NULL, 10);
+	*extra = (void *) myextra;
+
+	return true;
+}
+
 
 /*
  * get_descriptor(FILE *file) find any free slot for FILE pointer.
@@ -249,6 +323,7 @@ utl_file_fopen(PG_FUNCTION_ARGS)
 	FILE	   *file;
 	char	   *fullname;
 	int			d;
+	mode_t		oldmask;
 
 	NOT_NULL_ARG(0);
 	NOT_NULL_ARG(1);
@@ -307,6 +382,8 @@ utl_file_fopen(PG_FUNCTION_ARGS)
 	 * for oracle compatibility.
 	 */
 
+	oldmask = umask((mode_t) orafce_umask);
+
 #ifndef WIN32
 
 	file = fopen(fullname, mode);
@@ -327,6 +404,8 @@ utl_file_fopen(PG_FUNCTION_ARGS)
 		file = fopen(fullname, mode);
 
 #endif
+
+	umask(oldmask);
 
 	if (!file)
 		IO_EXCEPTION();

Makefile
@@ -30,7 +30,7 @@ OBJS= regexp.o\
 
 EXTENSION = orafce
 
-DATA = orafce--4.9.sql\
+DATA = orafce--4.10.sql\
 		orafce--3.2--3.3.sql\
 		orafce--3.3--3.4.sql\
 		orafce--3.4--3.5.sql\
@@ -63,7 +63,8 @@ DATA = orafce--4.9.sql\
 		orafce--4.5--4.6.sql\
 		orafce--4.6--4.7.sql\
 		orafce--4.7--4.8.sql\
-		orafce--4.8--4.9.sql
+		orafce--4.8--4.9.sql\
+		orafce--4.9--4.10.sql
 
 
 DOCS = README.asciidoc COPYRIGHT.orafce INSTALL.orafce

orafce--4.10.sql
@@ -4314,3 +4314,11 @@ select str;
 $$ LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE;
 COMMENT ON FUNCTION oracle.to_char(text) IS 'Convert string to string';
 
+DO $$
+BEGIN
+  IF NOT EXISTS(SELECT * FROM pg_roles where rolname = 'orafce_set_umask') THEN
+    CREATE ROLE orafce_set_umask INHERIT NOLOGIN;
+  END IF;
+END;
+$$;
+

orafce--4.9--4.10.sql
@@ -1,0 +1,7 @@
+DO $$
+BEGIN
+  IF NOT EXISTS(SELECT * FROM pg_roles where rolname = 'orafce_set_umask') THEN
+    CREATE ROLE orafce_set_umask INHERIT NOLOGIN;
+  END IF;
+END;
+$$;

orafce.c
@@ -162,6 +162,15 @@ _PG_init(void)
 							 PGC_USERSET, 0,
 							 NULL, NULL, NULL);
 
+	DefineCustomStringVariable("orafce.umask",
+									"Specify umask used by utl_file.fopen.",
+									NULL,
+									&orafce_umask_str,
+									"0077",
+									PGC_USERSET,
+									0,
+									orafce_umask_check_hook,
+									orafce_umask_assign_hook, NULL);
 
 	EmitWarningsOnPlaceholders("orafce");
 

orafce.control
@@ -1,5 +1,5 @@
 # orafce extension
 comment = 'Functions and operators that emulate a subset of functions and packages from the Oracle RDBMS'
-default_version = '4.9'
+default_version = '4.10'
 module_pathname = '$libdir/orafce'
 relocatable = false

orafce.h
@@ -8,6 +8,7 @@
 #include <sys/time.h>
 #include "utils/datetime.h"
 #include "utils/datum.h"
+#include "utils/guc.h"
 
 #define TextPCopy(t) \
 	DatumGetTextP(datumCopy(PointerGetDatum(t), false, -1))
@@ -41,12 +42,16 @@ extern int ora_mb_strlen1(text *str);
 
 extern char *nls_date_format;
 extern char *orafce_timezone;
+extern char *orafce_umask_str;
 
 extern bool orafce_varchar2_null_safe_concat;
 
 extern int orafce_substring_length_is_zero;
 
 extern void orafce_xact_cb(XactEvent event, void *arg);
+
+extern void orafce_umask_assign_hook(const char *newvalue, void *extra);
+extern bool orafce_umask_check_hook(char **newval, void **extra, GucSource source);
 
 
 /*

commit dfc83dd465e556265fc011302d21f7f343ce5fd4

Fixed incorrect length checking
Added missing increment of digit counter, the checking
of digits was always false.

Found by PostgresPro
Fixes: ae9d030 ("Allow to set umask for utl_file.fopen by setting orafce.umask.
This setting is allowed only for user with rights of role orafce_set_umask.")
Signed-off-by: Maksim Korotkov <m.korotkov@postgrespro.ru>

file.c
@@ -169,7 +169,7 @@ orafce_umask_check_hook(char **newval, void **extra, GucSource source)
 			GUC_check_errdetail("invalid octal digit");
 			return false;
 		}
-
+		digits++;
 		if (digits > 3)
 		{
 			GUC_check_errdetail("number is too big (only four digits are allowed");