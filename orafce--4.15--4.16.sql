6a3cfa5c54ae8e6d2b6ed9815723cf4875479164
okbob@github.com <pavel.stehule@gmail.com>
2025-11-08 08:52:19 +0100

    fix update script from 4.15-4.16 and prepare for 4.16.2

--- orafce--4.15--4.16.sql
+++ orafce--4.15--4.16.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION oracle.months_between(date1 oracle.date, date2 oracle.date)
 RETURNS numeric
-AS 'MODULE_PATHNAME', months_between_timestamp
+AS 'MODULE_PATHNAME', 'months_between_timestamp'
 LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 COMMENT ON FUNCTION oracle.months_between(oracle.date, oracle.date) IS 'returns the number of months between date1 and date2';
 
