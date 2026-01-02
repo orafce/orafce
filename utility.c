ef706f8b9f4d4562f329d43157ac858311d9c151
okbob@github.com <pavel.stehule@gmail.com>
2026-01-02 06:16:24 +0100

    update year in copyright

--- utility.c
+++ utility.c
@@ -3,7 +3,7 @@
   free available library PL/Vision. Please look www.quest.com
 
   Original author: Steven Feuerstein, 1996 - 2002
-  PostgreSQL implementation author: Pavel Stehule, 2006-2025
+  PostgreSQL implementation author: Pavel Stehule, 2006-2026
 
   This module is under BSD Licence
 
